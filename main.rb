473cb3f591891afb91f9674bc028fe6f26aac7c9
Richard Guay <raguay@customct.com>
2014-03-24 22:06:08 +0700

    Added ability to add new links from the Script Filter

--- main.rb
+++ main.rb
@@ -1,33 +1,82 @@
 require 'json'
 
+def notIn( links, name )
+	result = false
+	links["links"].each do |link|
+		if link["name"] == name
+			result = true
+		end
+	end
+	return ! result
+end
+
 bundleID = `/usr/libexec/PlistBuddy  -c "Print :bundleid" "info.plist"`.strip
 dataDir = "#{Dir.home()}/Library/Application Support/Alfred 2/Workflow Data/#{bundleID}"
-query = ARGV.join(' ').strip.downcase
+query = ARGV.join(' ').strip
 
-links = JSON.parse(File.read("#{dataDir}/links.json"))['links']
-
-if query != ''
-  links.reject! do |link|
-    name = link['name']
-
-    name.to_s.downcase.include?(query)
-  end
-end
+links = JSON.parse(File.read("#{dataDir}/links.json"))
 
 # http://www.alfredforum.com/topic/5-generating-feedback-in-workflows/
 output = %{<?xml version="1.0"?><items>}
 
-links.each do  |link|
-  name = link['name']
-  href = link['href']
+parts = query.split("|")
+nparts = parts.count
+r = /#{parts[0]}.*/i
+icount = 0
 
-  output += %{
-  <item uid="#{name}" arg="#{href}" autocomplete="#{name}">
-    <title>#{name}</title>
-    <subtitle>#{href}</subtitle>
-  <icon>link.png</icon>
-  </item>
-  }
+if nparts > 1
+	if nparts == 2
+		output += %{
+			<item uid="new" arg="" valid="no" autocomplete="#{parts[0]}|#{parts[1]}|n">
+			<title>New Link Address: #{parts[1]}</title>
+			<subtitle>Add new Link</subtitle>
+			<icon>link.png</icon>
+			</item>
+		}
+		icount += 1
+	else
+		if notIn(links,parts[0])
+			nlink = {}
+			nlink['name'] = parts[0]
+			nlink['href'] = parts[1]
+			links["links"].push(nlink)
+			File.write("#{dataDir}/links.json",links.to_json)
+		end
+		output += %{
+			<item uid="new" arg="#{parts[1]}" valid="yes" autocomplete="">
+			<title>New Link Name: #{parts[0]}</title>
+			<subtitle>New Link Address: #{parts[1]}</subtitle>
+			<icon>link.png</icon>
+			</item>
+		}
+		icount += 1
+	end
+else
+	links["links"].each do  |link|
+		name = link['name']
+		href = link['href']
+
+		if ! name.match(r).nil?
+			output += %{
+				<item uid="#{name}" arg="#{href}" autocomplete="#{name}">
+				<title>#{name}</title>
+				<subtitle>#{href}</subtitle>
+				<icon>link.png</icon>
+				</item>
+			}
+			icount += 1
+		end
+	end
+end
+
+if icount < 1
+	output += %{
+		<item uid="new" arg="" valid="no" autocomplete="#{parts[0]}|http://">
+		<title>Link Name:  #{parts[0]}</title>
+		<subtitle>Create a new Link</subtitle>
+		<icon>link.png</icon>
+		</item>
+	}
 end
 
 output += '</items>'
